3464d308190ce049c9872285d0b6ad19e8bd9a8a
KozlowskiKamil <kamil2k8@gmail.com>
2023-04-01 21:02:13 +0200

    zadanie ok

--- src/main/java/com/infoshareacademy/Zad5.java
+++ src/main/java/com/infoshareacademy/Zad5.java
@@ -7,9 +7,9 @@ public class Zad5 {
     public static void main(String[] args) {
 
         List<String> list = new ArrayList<>(List.of("Kamil", "Ola", "Kuba", "Kasia", "Daniel"));
-        list.forEach(n -> System.out.println("n = " + n));
-        list.forEach(n -> System.out.println("n = " + n.toUpperCase()));
-        list.forEach(n -> System.out.println("n = " + n.charAt(0)));
+        list.forEach(s -> System.out.println("s = " + s));
+        list.forEach(s -> System.out.println("s = " + s.toUpperCase()));
+        list.forEach(s -> System.out.println("s = " + s.charAt(0)));
 
     }
 }
